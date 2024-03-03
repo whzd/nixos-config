8f98d2af6c4a78de72a5efd01b1a824311f38212
André Dias <whzdxl@gmail.com>
2024-03-03 23:39:21 +0000

    Add default tmux session on terminal init

--- user/shell/zsh.nix
+++ user/shell/zsh.nix
@@ -12,8 +12,15 @@
       ''
       	# Remove anoying beep
         unsetopt beep
+
 	# Bind autosuggestions complete
 	bindkey '^ ' autosuggest-accept
+
+	# Initiate tmux session on open
+	if [[ "$TERM" != "screen-256color" ]]
+	then
+	  tmux attach-session -t "$USER" || tmux new-session -s "$USER"
+	fi
       '';
 
     oh-my-zsh =
